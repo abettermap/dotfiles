c267cccc250c2e0442c560ca3add35ea88813fa4
Jason Lampel <travelampel@gmail.com>
2017-02-25 11:59:38 -0700

    Include zshrc in symlink generation

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc gitconfig bash_profile profile vimrc vim profile npmrc"    # list of files/folders to symlink in homedir
+files="bashrc gitconfig bash_profile profile vimrc vim profile npmrc zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
